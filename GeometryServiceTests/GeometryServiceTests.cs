c64cdc58abda76b6cfbbcc33780c1330b41b5b20
Garrett Carver <gariti@gmail.com>
2019-01-21 18:01:59 -0800

    added/passed 2nd test (A2 triangle)

--- GeometryServiceTests/GeometryServiceTests.cs
+++ GeometryServiceTests/GeometryServiceTests.cs
@@ -31,5 +31,31 @@ namespace gariti.Geometry.Tests
             Assert.IsTrue(triangle.Point2.X == 0 && triangle.Point2.Y == 10);
             Assert.IsTrue(triangle.Point3.X == 10 && triangle.Point3.Y == 10);
         }
+
+        [TestMethod]
+        public void ShouldGetA2TriangleVerticies()
+        {
+            //Arrange
+            //Coordinates represent offset from top left of the grid:
+            //X = right offset
+            //Y = down offset
+
+            //x & y offset represents the upper-left coordinates of square
+            int xSquarePos = 0;
+            int ySquarePos = 0;
+
+            //Act
+            Triangle triangle = TriangleHelper.GetTopTriangle(xSquarePos, ySquarePos, Constants.SQUARE_SIZE);
+
+            //Assert
+
+            //Upper Left corner - 0 , 0
+            //Upper Right corner - 10 , 0
+            //Lower Right corner - 10 , 10
+
+            Assert.IsTrue(triangle.Point1.X == 0 && triangle.Point1.Y == 0);
+            Assert.IsTrue(triangle.Point2.X == 10 && triangle.Point2.Y == 0);
+            Assert.IsTrue(triangle.Point3.X == 10 && triangle.Point3.Y == 10);
+        }
     }
 }
--- gariti.Geometry.Utilities/TriangleHelper.cs
+++ gariti.Geometry.Utilities/TriangleHelper.cs
@@ -18,5 +18,19 @@ namespace gariti.Geometry.Utilities
                 Point3 = new Point(xOffset + squareSize, yOffset + squareSize)
             };
         }
+
+        public static Triangle GetTopTriangle(int xOffset, int yOffset, int squareSize)
+        {
+            return new Triangle()
+            {
+                //Point1 - Upperleft
+                //Point2 - UpperRight
+                //Point3 - Bottomright
+
+                Point1 = new Point(xOffset, yOffset),
+                Point2 = new Point(xOffset + squareSize, yOffset),
+                Point3 = new Point(xOffset + squareSize, yOffset + squareSize)
+            };
+        }
     }
 }
