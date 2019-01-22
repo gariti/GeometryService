a20b8e9a62f80fd33b5fdf2a2440413395a3ccb6
Garrett Carver <gariti@gmail.com>
2019-01-21 18:53:19 -0800

    added/passed b1,b2 test cases

--- GeometryServiceTests/GeometryServiceTests.cs
+++ GeometryServiceTests/GeometryServiceTests.cs
@@ -57,5 +57,38 @@ namespace gariti.Geometry.Tests
             Assert.IsTrue(triangle.Point2.X == 10 && triangle.Point2.Y == 0);
             Assert.IsTrue(triangle.Point3.X == 10 && triangle.Point3.Y == 10);
         }
+
+        [TestMethod]
+        public void ShouldGetB1TriangleVerticies()
+        {
+            //Arrange
+            int xSquarePos = 0;
+            int ySquarePos = 10;
+
+            //Act
+            Triangle triangle = TriangleHelper.GetBottomTriangle(xSquarePos, ySquarePos, Constants.SQUARE_SIZE);
+
+            //Assert
+            Assert.IsTrue(triangle.Point1.X == 0 && triangle.Point1.Y == 10);
+            Assert.IsTrue(triangle.Point2.X == 0 && triangle.Point2.Y == 20);
+            Assert.IsTrue(triangle.Point3.X == 10 && triangle.Point3.Y == 20);
+        }
+
+        [TestMethod]
+        public void ShouldGetB2TriangleVerticies()
+        {
+            //Arrange
+            int xSquarePos = 0;
+            int ySquarePos = 10;
+
+            //Act
+            Triangle triangle = TriangleHelper.GetTopTriangle(xSquarePos, ySquarePos, Constants.SQUARE_SIZE);
+
+            //Assert
+            Assert.IsTrue(triangle.Point1.X == 0 && triangle.Point1.Y == 10);
+            Assert.IsTrue(triangle.Point2.X == 10 && triangle.Point2.Y == 10);
+            Assert.IsTrue(triangle.Point3.X == 10 && triangle.Point3.Y == 20);
+        }
+
     }
 }
