a86ee62abf1e89dfce1da50cff1b9a805acb1f4d
Garrett Carver <gariti@gmail.com>
2019-01-24 21:48:01 -0800

    modified gridcontroller to get triangle via points or by location

--- gariti.Geometry.Service/Controllers/GridController.cs
+++ gariti.Geometry.Service/Controllers/GridController.cs
@@ -10,7 +10,7 @@ namespace gariti.Geometry.API.Controllers
 {
     public class GridController : Controller
     {
-        [HttpGet("api/grid/gettriangle")]
+        [HttpGet("api/grid/triangle/byPoints")]
         //example query string localhost:44328/api/grid/gettriangle?x1=0&y1=0&x2=0&y2=10&x3=10&y3=10 returns "A1"
         public JsonResult GetTriangleRowAndColumn([FromQuery]int x1, [FromQuery]int y1, [FromQuery]int x2, [FromQuery]int y2, [FromQuery]int x3, [FromQuery]int y3)
         {
@@ -22,5 +22,16 @@ namespace gariti.Geometry.API.Controllers
 
             return new JsonResult(grid.CalculateTriangleRowAndColumn(point1, point2, point3));
         }
+
+        [HttpGet("api/grid/triangle/byLoc")]
+        public JsonResult GetTrianglePoints([FromQuery] string loc)
+        {
+            Grid grid = new Grid(Constants.GRID_SIZE, Constants.GRID_SIZE, Constants.SQUARE_SIZE);
+
+            Triangle triangle = grid.GetTriangle(loc);
+
+            return new JsonResult(triangle);
+        }
+
     }
 }
